8dfe368d1aa63f2b09e648e1ea93868dfb58f0f7
MinSoo Kim <misol.kr@gmail.com>
2014-10-31 11:35:35 +0900

    maps module 기초 제작중 2

--- maps.admin.model.php
+++ maps.admin.model.php
@@ -103,6 +103,18 @@ class mapsAdminModel extends maps
 
 	}
 
+	/**
+	 * @brief 주소 검색 반환. 각 사이트의 API 이용.
+	 * @author MinSoo Kim (misol.kr@gmail.com)
+	 * @param string $query 검색 쿼리.
+	 * @todo 자체 DB 기능을 추가할 경우 자체 DB 중 위치도 포함해서 검색.
+	 */
+	public function getMapsAdminList($args)
+	{
+		$output = executeQuery('maps.getMapsAdminList', $args);
+		return $output;
+	}
+
 }
 /* End of file maps.admin.model.php */
 /* Location: ./modules/maps/maps.admin.model.php */
--- maps.admin.view.php
+++ maps.admin.view.php
@@ -20,10 +20,49 @@ class mapsAdminView extends maps
 	{
 		// option to get a list
 		$args = new stdClass();
+		$args->page = intval(Context::get('page'));
+		$args->order_type = 'desc';// asc | desc
+		$oMapsAdminModel = getAdminModel('maps');
+		$maps_list = $oMapsAdminModel->getMapsAdminList($args);
+
+		//Pre. page
+		if($maps_list->page_navigation->first_page <= $maps_list->page_navigation->cur_page - 1)
+		{
+			$maps_list->page_navigation->prev_page = $maps_list->page_navigation->cur_page - 1;
+		}
+		else
+		{
+			$maps_list->page_navigation->prev_page = $maps_list->page_navigation->cur_page;
+		}
+
+
+
+		Context::set('total_count', $maps_list->total_count);
+		Context::set('total_page', $maps_list->total_page);
+		Context::set('total_page', $maps_list->total_page);
+		Context::set('page', $maps_list->page);
+		Context::set('page_navigation', $maps_list->page_navigation);
+		Context::set('maps_list', $maps_list->data);
 
 		// Specify a template
 		$this->setTemplatePath($this->module_path.'tpl');
 		$this->setTemplateFile('maps_list');
+	}
+
+	public function dispMapsAdminWrite()
+	{
+		Context::get('maps_srl');
+
+		// Specify a template
+		$this->setTemplatePath($this->module_path.'tpl');
+		$this->setTemplateFile('maps_write');
+	}
+
+	public function dispMapsAdminConfig()
+	{
+		// Specify a template
+		$this->setTemplatePath($this->module_path.'tpl');
+		$this->setTemplateFile('maps_config');
 	}
 }
 /* End of file maps.admin.view.php */
